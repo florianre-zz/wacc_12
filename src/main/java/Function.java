e862d8ec807bfeca8d78c7c62c3a6090deb15f99
fre14 <femile@blueyonder.co.uk>
2015-11-06 23:03:19 +0000

    [FE][EA] Create Variable and Function classes. They will be used in our symbol table to store any functions and variables that are created with their associated bindings (type, return type, param types...).

---
+++ src/main/java/Function.java
@@ -0,0 +1,29 @@
+import antlr.SymbolTable;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+public class Function extends Bindings {
+
+  private Type type;
+  private Variable[] params;
+  private SymbolTable symbolTable;
+
+  public Function(String name, ParserRuleContext ctx, Variable[] params,
+                  SymbolTable symbolTable, Type type) {
+    super(name, ctx);
+    this.params = params;
+    this.symbolTable = symbolTable;
+    this.type = type;
+  }
+
+  public Type getType() {
+    return type;
+  }
+
+  public Variable[] getParams() {
+    return params;
+  }
+
+  public SymbolTable getSymbolTable() {
+    return symbolTable;
+  }
+}
