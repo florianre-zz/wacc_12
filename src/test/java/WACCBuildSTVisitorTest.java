66ae9a8b502d33c2e329891ae65a80f043618e73
fre14 <femile@blueyonder.co.uk>
2015-11-09 12:26:06 +0000

    [FE][EA][EG] Implement first test that checks that chekcs that progSymbTable is in top.

--- src/test/java/WACCBuildSTVisitorTest.java
+++ src/test/java/WACCBuildSTVisitorTest.java
@@ -2,17 +2,35 @@
 // import static org.hamcrest.CoreMatchers.hasItems;
 // import static org.junit.Assert.fail;
 
+import antlr.WACCParser;
+import bindings.NewScope;
+import bindings.SymbolTable;
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.Rule;
 import org.junit.Test;
+import wacc.WACCBuildSTVisitor;
 
 public class WACCBuildSTVisitorTest {
 
   @Rule
-  public JUnitRuleMockery context = new JUnitRuleMockery();
+  public JUnitRuleMockery context = new JUnitRuleMockery() {{
+    setImposteriser(ClassImposteriser.INSTANCE);
+  }};
+  final SymbolTable top = context.mock(SymbolTable.class);
 
   @Test
   public void visitProgAddsNewProgramSymbolTableToTop(){
-    // TODO: this
+    final WACCParser.ProgContext ctx
+        = context.mock(WACCParser.ProgContext.class);
+    context.checking(new Expectations() {{
+      oneOf(top).add(with(aNonNull(String.class)), with(aNonNull(NewScope
+                                                                   .class)));
+      ignoring(ctx).getChildCount();
+    }});
+    WACCBuildSTVisitor buildSTVisitor = new WACCBuildSTVisitor(top);
+    buildSTVisitor.visitProg(ctx);
   }
+  
 }
