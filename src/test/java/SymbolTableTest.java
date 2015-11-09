45292af2558d41ede7f7bbf3b584610ecf079219
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2015-11-09 17:21:24 +0000

    Test lookupAll

--- src/test/java/SymbolTableTest.java
+++ src/test/java/SymbolTableTest.java
@@ -12,27 +12,58 @@ import static org.junit.Assert.assertThat;
 
 public class SymbolTableTest {
 
+  public static final String TEST_KEY = "testKey";
   @Rule
   public JUnitRuleMockery context = new JUnitRuleMockery() {{
     setImposteriser(ClassImposteriser.INSTANCE);
   }};
 
-  SymbolTable<String, Binding> symbolTable = new SymbolTable<>();
+  private SymbolTable<String, Binding> symbolTable = new SymbolTable<>();
+  private SymbolTable<String, Binding> parentSymbolTable = new SymbolTable<>();
+  private Binding binding = context.mock(Binding.class, "binding");
 
   @Test
-  public void testLookupAll() throws Exception {
+  public void testLookupAllNull() throws Exception {
+    assertNull(symbolTable.lookupAll(TEST_KEY));
+  }
 
+  @Test
+  public void testLookupAllInChildNoParent() throws Exception {
+    symbolTable.put(TEST_KEY, binding);
+    assertThat(symbolTable.lookupAll(TEST_KEY), is(binding));
+  }
+
+  @Test
+  public void testLookupAllInChildWithParent() throws Exception {
+    symbolTable = new SymbolTable<>(parentSymbolTable);
+    symbolTable.put(TEST_KEY, binding);
+    assertThat(symbolTable.lookupAll(TEST_KEY), is(binding));
+  }
+
+  @Test
+  public void testLookupAllInChildAndParent() throws Exception {
+    symbolTable = new SymbolTable<>(parentSymbolTable);
+    symbolTable.put(TEST_KEY, binding);
+    Binding parentBinding = context.mock(Binding.class, "parentBinding");
+    parentSymbolTable.put(TEST_KEY, parentBinding);
+    assertThat(symbolTable.lookupAll(TEST_KEY), is(binding));
+  }
+
+  @Test
+  public void testLookupAllInParentNotChild() throws Exception {
+    symbolTable = new SymbolTable<>(parentSymbolTable);
+    parentSymbolTable.put(TEST_KEY, binding);
+    assertThat(symbolTable.lookupAll(TEST_KEY), is(binding));
   }
 
   @Test
   public void testGetEnclosingSTNull() throws Exception {
-    SymbolTable<String, Binding> st = new SymbolTable<>();
-    assertNull(st.getEnclosingST());
+    assertNull(symbolTable.getEnclosingST());
   }
 
   @Test
   public void testGetEnclosingSTParent() throws Exception {
-    SymbolTable<String, Binding> st = new SymbolTable<>(symbolTable);
-    assertThat(st.getEnclosingST(), is(symbolTable));
+    symbolTable = new SymbolTable<>(parentSymbolTable);
+    assertThat(symbolTable.getEnclosingST(), is(parentSymbolTable));
   }
 }
