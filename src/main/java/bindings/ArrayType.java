d1b09b427fb10196ee452e3b92efce7718bd8bcb
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2015-11-13 19:59:44 +0000

    [EG][JK] Add dimensionality

--- src/main/java/bindings/ArrayType.java
+++ src/main/java/bindings/ArrayType.java
@@ -3,9 +3,21 @@ package bindings;
 public class ArrayType extends Type {
 
   private Type type;
+  private int dimensionality;
 
   public ArrayType(Type type) {
     super(type.getName());
+    dimensionality = 1;
+    if (type instanceof ArrayType) {
+      dimensionality += ((ArrayType) type).getDimensionality();
+    }
+    this.type = type;
+  }
+
+  public ArrayType(Type type, int dimensionality) {
+    super(type.getName());
+    assert dimensionality > 0 : "Dimensionality must be greater than 0";
+    this.dimensionality = dimensionality;
     this.type = type;
   }
 
@@ -16,6 +28,10 @@ public class ArrayType extends Type {
   @Override
   public String toString() {
     return type.toString() + "[]";
+  }
+
+  public int getDimensionality() {
+    return dimensionality;
   }
 
 }
