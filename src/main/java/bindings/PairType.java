f9b456c7058442a04565f34c180367807b679dc1
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2015-11-16 11:52:00 +0000

    [EG][JK] Change 'parameterised' to 'generic'

--- src/main/java/bindings/PairType.java
+++ src/main/java/bindings/PairType.java
@@ -3,7 +3,7 @@ package bindings;
 public class PairType extends Type {
 
   private Type fst, snd;
-  private boolean parameterised = false;
+  private boolean generic = true;
 
   public PairType() {
     super(Types.PAIR_T);
@@ -11,7 +11,7 @@ public class PairType extends Type {
 
   public PairType(Type fst, Type snd) {
     this();
-    parameterised = true;
+    generic = false;
     this.fst = fst;
     this.snd = snd;
   }
@@ -19,7 +19,7 @@ public class PairType extends Type {
   @Override
   public String toString() {
 
-    if (!parameterised) {
+    if (generic) {
       return super.toString();
     }
 
@@ -44,26 +44,18 @@ public class PairType extends Type {
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
-    if (!super.equals(o)) return false;
 
     PairType type = (PairType) o;
 
-    if(!(type.parameterised && parameterised)) {
+    if(type.generic || generic) {
       return true;
     }
 
-    if (fst != null ? !fst.equals(type.fst) : type.fst != null) return false;
-    if (snd != null ? !snd.equals(type.snd) : type.snd != null) return false;
-
-    return true;
+    return super.equals(o);
   }
 
   @Override
   public int hashCode() {
-    int result = super.hashCode();
-    result = 31 * result + (fst != null ? fst.hashCode() : 0);
-    result = 31 * result + (snd != null ? snd.hashCode() : 0);
-    result = 31 * result + (parameterised ? 1 : 0);
-    return result;
+    return super.hashCode();
   }
 }
