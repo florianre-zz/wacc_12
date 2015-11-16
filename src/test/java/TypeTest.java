3b81d3d07638f2f4ce595e894b0453923c15b9b2
Jonathan King <jonathanlking@me.com>
2015-11-16 18:59:41 +0000

    Add testStringIsCharArray + testCharArrayIsString tests

--- src/test/java/TypeTest.java
+++ src/test/java/TypeTest.java
@@ -2,6 +2,7 @@ import bindings.*;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
@@ -132,6 +133,22 @@ public class TypeTest {
     PairType pairType = new PairType();
     assertEquals(new PairType(), new PairType(pairType, pairType));
     assertEquals(new PairType(pairType, pairType), new PairType());
+  }
+
+  @Test
+  public void testStringIsCharArray() {
+    Type charArray = new ArrayType(new Type(Types.CHAR_T));
+    Type string = new Type(Types.STRING_T);
+
+    assert string.equals(charArray);
+  }
+
+  @Test
+  public void testCharArrayIsString() {
+    Type charArray = new ArrayType(new Type(Types.CHAR_T));
+    Type string = new Type(Types.STRING_T);
+
+    assert charArray.equals(string);
   }
 
 }
