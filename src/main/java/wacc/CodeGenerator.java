f16953637f76af9ed26013ab80a8a2dd9ee1c16e
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2015-11-20 20:41:36 +0000

    Implement visitMain, visitSkip, visitExit

--- src/main/java/wacc/CodeGenerator.java
+++ src/main/java/wacc/CodeGenerator.java
@@ -1,9 +1,56 @@
 package wacc;
 
+import antlr.WACCParser;
 import antlr.WACCParserBaseVisitor;
-import instructions.Instruction;
+import arm11.*;
+import org.antlr.v4.runtime.misc.NotNull;
 
-import java.util.List;
+public class CodeGenerator extends WACCParserBaseVisitor<InstructionList> {
 
-public class CodeGenerator extends WACCParserBaseVisitor<List<Instruction>> {
+  @Override
+  public InstructionList visitMain(@NotNull WACCParser.MainContext ctx) {
+
+    InstructionList list = new InstructionList();
+
+    Label label = new Label(WACCVisitor.Scope.MAIN.toString());
+    list.add(InstructionFactory.createLabel(label));
+
+    Register register = ARM11Registers.getRegister(14);
+    list.add(InstructionFactory.createPush(register));
+
+    list.add(visitChildren(ctx));
+
+    Register R0 = ARM11Registers.getRegister(0);
+    Operand value = new Immediate((long) 0);
+    list.add(InstructionFactory.createLoad(R0, value));
+
+    register = ARM11Registers.getRegister(15);
+    list.add(InstructionFactory.createPop(register));
+
+    list.add(InstructionFactory.createLTORG());
+
+    return super.visitMain(ctx);
+  }
+
+  @Override
+  public InstructionList visitSkipStat(
+      @NotNull WACCParser.SkipStatContext ctx) {
+    return new InstructionList();
+  }
+
+  @Override
+  public InstructionList visitExitStat(
+      @NotNull WACCParser.ExitStatContext ctx) {
+    InstructionList list = new InstructionList();
+
+    Register R0 = ARM11Registers.getRegister(0);
+    Long imm = Long.parseLong(ctx.expr().getText());
+    Operand value = new Immediate(imm);
+    Label label = new Label("");
+
+    list.add(InstructionFactory.createLoad(R0, value));
+    list.add(InstructionFactory.createBranchLink(label));
+
+    return list;
+  }
 }
