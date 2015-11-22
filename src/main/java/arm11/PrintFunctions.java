e5b7c23accd177e104630a6e6b753909d28f87f1
fre14 <femile@blueyonder.co.uk>
2015-11-22 19:27:15 +0000

    [FE][JK] Implement printString(), printLn(), printBool()

--- src/main/java/arm11/PrintFunctions.java
+++ src/main/java/arm11/PrintFunctions.java
@@ -3,6 +3,7 @@ package arm11;
 // TODO: in main move argument to r0
 
 public class PrintFunctions {
+
     public static InstructionList printInt(String formatterLabel) {
       // Where label is the name of the data section which has the int
       // formatter string
@@ -12,8 +13,8 @@ public class PrintFunctions {
       //		    .ascii	"%d\0"
 
       InstructionList list = new InstructionList();
-      Label label = new Label("p_print-int");
 
+      Label label = new Label("p_print_int");
       Register r0 = ARM11Registers.getRegister(ARM11Registers.ARM11Register.R0);
       Register r1 = ARM11Registers.getRegister(ARM11Registers.ARM11Register.R1);
       Register lr = ARM11Registers.getRegister(ARM11Registers.ARM11Register.LR);
@@ -41,4 +42,102 @@ public class PrintFunctions {
 
       return list;
     }
+
+  public static InstructionList printString(String formatterLabel) {
+    InstructionList list = new InstructionList();
+
+    Label label = new Label("p_print_string");
+    Register r0 = ARM11Registers.getRegister(ARM11Registers.ARM11Register.R0);
+    Register r2 = ARM11Registers.getRegister(ARM11Registers.ARM11Register.R2);
+    Register lr = ARM11Registers.getRegister(ARM11Registers.ARM11Register.LR);
+    Register pc = ARM11Registers.getRegister(ARM11Registers.ARM11Register.PC);
+
+//    p_print_string:
+//    PUSH {lr}
+//    LDR r1, [r0]
+//    ADD r2, r0, #4
+//    LDR r0, =!msg_2!
+//        ADD r0, r0, #4
+//    BL printf
+//    MOV r0, #0
+//    BL fflush
+//    POP {pc}
+
+    list.add(InstructionFactory.createLabel(label));
+    list.add(InstructionFactory.createPush(lr));
+    //list.add(InstructionFactory.createMov(r1, r0));
+    list.add(InstructionFactory.createAdd(r2, r0, 4));
+    list.add(InstructionFactory.createLoad(r0, new Label(formatterLabel)));
+    list.add(InstructionFactory.createAdd(r0, r0, 4));
+    list.add(InstructionFactory.createBranchLink(new Label("printf")));
+    list.add(InstructionFactory.createMov(r0, 0));
+    list.add(InstructionFactory.createBranchLink(new Label("fflush")));
+    list.add(InstructionFactory.createPop(pc));
+
+    return list;
+  }
+
+  public static InstructionList printLn() {
+    InstructionList list = new InstructionList();
+
+    Label label = new Label("p_print_ln");
+    Register r0 = ARM11Registers.getRegister(ARM11Registers.ARM11Register.R0);
+    Register lr = ARM11Registers.getRegister(ARM11Registers.ARM11Register.LR);
+    Register pc = ARM11Registers.getRegister(ARM11Registers.ARM11Register.PC);
+
+//    p_print_ln:
+//    PUSH {lr}
+//    LDR r0, =!msg_3!
+//        ADD r0, r0, #4
+//    BL puts
+//    MOV r0, #0
+//    BL fflush
+//    POP {pc}
+
+    list.add(InstructionFactory.createLabel(label));
+    list.add(InstructionFactory.createPush(lr));
+    list.add(InstructionFactory.createLoad(r0, new Label("\0")));
+    list.add(InstructionFactory.createAdd(r0, r0, 4));
+    list.add(InstructionFactory.createBranchLink(new Label("puts")));
+    list.add(InstructionFactory.createMov(r0, 0));
+    list.add(InstructionFactory.createBranchLink(new Label("fflush")));
+    list.add(InstructionFactory.createPop(pc));
+
+    return list;
+  }
+
+  public static InstructionList printBool(String formatterLabel1,
+                                          String formatterLabel2) {
+    InstructionList list = new InstructionList();
+
+    Label label = new Label("p_print_bool");
+    Register r0 = ARM11Registers.getRegister(ARM11Registers.ARM11Register.R0);
+    Register lr = ARM11Registers.getRegister(ARM11Registers.ARM11Register.LR);
+    Register pc = ARM11Registers.getRegister(ARM11Registers.ARM11Register.PC);
+
+//    p_print_bool:
+//    PUSH {lr}
+//    CMP r0, #0
+//    LDRNE r0, =msg_3
+//    LDREQ r0, =msg_4
+//    ADD r0, r0, #4
+//    BL printf
+//    MOV r0, #0
+//    BL fflush
+//    POP {pc}
+
+    list.add(InstructionFactory.createLabel(label));
+    list.add(InstructionFactory.createPush(lr));
+    //cmp
+    //ldrne
+    //ldreq
+    list.add(InstructionFactory.createAdd(r0, r0, 4));
+    list.add(InstructionFactory.createBranchLink(new Label("printf")));
+    list.add(InstructionFactory.createMov(r0, 0));
+    list.add(InstructionFactory.createBranchLink(new Label("fflush")));
+    list.add(InstructionFactory.createPop(pc));
+
+    return list;
+  }
+
 }
