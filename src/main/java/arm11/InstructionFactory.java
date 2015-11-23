eb1a2ad95a8cf162b912f1eb94498e6424ddcfef
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2015-11-23 17:30:47 +0000

    [EG][EA] Implement createStore

--- src/main/java/arm11/InstructionFactory.java
+++ src/main/java/arm11/InstructionFactory.java
@@ -223,6 +223,28 @@ public class InstructionFactory {
       }
     };
   }
+
+  public static Instruction createStore(Register reg,
+                                        Register base,
+                                        Operand offset) {
+    List<Operand> operands = new ArrayList<>();
+    operands.add(reg);
+    operands.add(base);
+    operands.add(offset);
+    return new Instruction(InstructionType.STR, operands) {
+      @Override
+      protected String printInstruction() {
+        StringBuilder sb = new StringBuilder(type.toString());
+        sb.append(" ").append(operands.get(0));
+        sb.append(", [").append(operands.get(1));
+        if (!operands.get(2).toString().equals("0")) {
+          sb.append(", #").append(operands.get(2));
+        }
+        sb.append("]");
+        return sb.toString();
+      }
+    };
+  }
 }
 
 
