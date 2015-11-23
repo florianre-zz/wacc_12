eb2e0118403d8975d9676abb73924a741d691673
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2015-11-23 13:59:23 +0000

    [EG][EA] allocate and deallocate space on the stack

--- src/main/java/wacc/CodeGenerator.java
+++ src/main/java/wacc/CodeGenerator.java
@@ -1,36 +1,51 @@
 package wacc;
 
 import antlr.WACCParser;
-import antlr.WACCParserBaseVisitor;
 import arm11.*;
+import bindings.Binding;
+import bindings.Variable;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.List;
 
 // TODO: review codestyle check errors
 // TODO: Do we need top for labels?
 
-public class CodeGenerator extends WACCParserBaseVisitor<InstructionList> {
+public class CodeGenerator extends WACCVisitor<InstructionList> {
+
+  public CodeGenerator(SymbolTable top) {
+    super(top);
+  }
 
   @Override
   public InstructionList visitProg(WACCParser.ProgContext ctx) {
+    String scopeName = Scope.PROG.toString();
+    changeWorkingSymbolTableTo(scopeName);
     InstructionList program = new InstructionList();
     program.add(InstructionFactory.createText());
     Label mainLabel = new Label(WACCVisitor.Scope.MAIN.toString());
     program.add(InstructionFactory.createGlobal(mainLabel));
     program.add(visitMain(ctx.main()));
+    goUpWorkingSymbolTable();
     return program;
   }
 
   @Override
   public InstructionList visitMain(WACCParser.MainContext ctx) {
+    String scopeName = Scope.MAIN.toString();
+    changeWorkingSymbolTableTo(scopeName);
 
     InstructionList list = new InstructionList();
 
-    Label label = new Label(WACCVisitor.Scope.MAIN.toString());
+    Label label = new Label(Scope.MAIN.toString());
     list.add(InstructionFactory.createLabel(label));
 
     Register register = ARM11Registers.getRegister(14);
     list.add(InstructionFactory.createPush(register));
 
+    list.add(allocateSpaceOnStack());
     list.add(visitChildren(ctx));
+    list.add(deallocateSpaceOnStack());
 
     Register R0 = ARM11Registers.getRegister(0);
     Operand value = new Immediate((long) 0);
@@ -40,6 +55,44 @@ public class CodeGenerator extends WACCParserBaseVisitor<InstructionList> {
     list.add(InstructionFactory.createPop(register));
 
     list.add(InstructionFactory.createLTORG());
+
+    goUpWorkingSymbolTable();
+    return list;
+  }
+
+  private InstructionList allocateSpaceOnStack() {
+    InstructionList list = new InstructionList();
+    List<Binding> variables = workingSymbolTable.filterByClass(Variable.class);
+    long stackSpaceSize = 0;
+    for (Binding b : variables) {
+      Variable v = (Variable) b;
+      stackSpaceSize += v.getType().getSize();
+    }
+    // TODO: deal with '4095', the max size... of something
+    if (stackSpaceSize > 0) {
+      Operand imm = new Immediate(stackSpaceSize);
+      Register sp = ARM11Registers.getRegister(13);
+      list.add(InstructionFactory.createSub(sp, sp, imm));
+    }
+
+    return list;
+  }
+
+
+  private InstructionList deallocateSpaceOnStack() {
+    InstructionList list = new InstructionList();
+    List<Binding> variables = workingSymbolTable.filterByClass(Variable.class);
+    long stackSpaceSize = 0;
+    for (Binding b : variables) {
+      Variable v = (Variable) b;
+      stackSpaceSize += v.getType().getSize();
+    }
+
+    if (stackSpaceSize > 0) {
+      Operand imm = new Immediate(stackSpaceSize);
+      Register sp = ARM11Registers.getRegister(13);
+      list.add(InstructionFactory.createAdd(sp, sp, imm));
+    }
 
     return list;
   }
@@ -64,4 +117,11 @@ public class CodeGenerator extends WACCParserBaseVisitor<InstructionList> {
     return list;
   }
 
+  @Override
+  public InstructionList visitInitStat(@NotNull WACCParser.InitStatContext ctx) {
+    // TODO: put value in a free register
+    // TODO: move what's in that register to the stack with the offset for this value
+    // TODO: change return value
+    return new InstructionList();
+  }
 }
--- src/main/java/wacc/WACCCompile.java
+++ src/main/java/wacc/WACCCompile.java
@@ -22,11 +22,12 @@ public class WACCCompile {
 
     ParseTree tree = new WACCParser(tokens).prog();
 
-    performSemanticAnalysis(tree, errorHandler);
+    SymbolTable<String, Binding> top
+        = performSemanticAnalysis(tree, errorHandler);
 
     checkForErrors(errorHandler);
 
-    performCodeGeneration(tree);
+    performCodeGeneration(tree, top);
 
   }
 
@@ -36,7 +37,8 @@ public class WACCCompile {
     return new CommonTokenStream(lexer);
   }
 
-  private static void performSemanticAnalysis(ParseTree tree,
+  private static SymbolTable<String, Binding> performSemanticAnalysis(
+                                              ParseTree tree,
                                               WACCErrorHandler errorHandler) {
 
 //    int numberOfSyntaxErrors = parser.getNumberOfSyntaxErrors();
@@ -54,6 +56,7 @@ public class WACCCompile {
     WACCTypeChecker typeChecker = new WACCTypeChecker(top, errorHandler);
     typeChecker.visit(tree);
 
+    return top;
   }
 
   private static SymbolTable<String, Binding> createTopSymbolTable() {
@@ -78,8 +81,9 @@ public class WACCCompile {
     }
   }
 
-  private static void performCodeGeneration(ParseTree tree) {
-    CodeGenerator codeGenerator = new CodeGenerator();
+  private static void performCodeGeneration(ParseTree tree,
+                                            SymbolTable<String, Binding> top) {
+    CodeGenerator codeGenerator = new CodeGenerator(top);
     InstructionList program = codeGenerator.visit(tree);
     System.out.println(program);
   }
