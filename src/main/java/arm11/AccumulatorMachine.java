b36e3bd19d58e10e4495e69cde57f60ea4ccf625
elyasaddo <les_a2do@yahoo.co.uk>
2015-12-02 22:15:47 +0000

    Implement method for creating ADD instruction

--- src/main/java/arm11/AccumulatorMachine.java
+++ src/main/java/arm11/AccumulatorMachine.java
@@ -1,10 +1,13 @@
 package arm11;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+
 import java.util.Stack;
 
 public class AccumulatorMachine {
 
     private static final Register ACCUMULATOR = ARM11Registers.R10;
+    private static final Register RESERVED = ARM11Registers.R11;
     
     private int regOverflowCount;
     private Stack<Register> freeRegisters;
@@ -29,12 +32,12 @@ public class AccumulatorMachine {
 
     public Register peekFreeRegister() {
         Register register = freeRegisters.peek();
-        return (register == ARM11Registers.R11 ? ACCUMULATOR : register);
+        return (register == RESERVED ? ACCUMULATOR : register);
     }
 
     public Register popFreeRegister() {
         Register register = freeRegisters.peek();
-        if (register == ARM11Registers.R11) {
+        if (register == RESERVED) {
             register = ACCUMULATOR;
             regOverflowCount++;
         } else {
@@ -69,5 +72,109 @@ public class AccumulatorMachine {
 //    }
 //  }
 
+
+    public InstructionList getInstructionList (InstructionType inst, Register dst, Register src, Operand op){
+        InstructionList result = new InstructionList();
+        if (inst.isArithmetic()) {
+            return arithmeticInstructions(inst, dst, src, op);
+        } else if (inst.isLogical()) {
+            return logicalInstructions(inst, dst, src, op);
+        } else if (inst.isMove()) {
+            return movInstructions(inst, dst, op);
+        } else if (inst.isLoad()) {
+            return loadInstructions(inst, dst, op);
+        }
+        return result;
+    }
+
+    private InstructionList loadInstructions(InstructionType inst, Register dst, Operand op) {
+        switch (inst) {
+            case LDR:
+                if (inAccumulatorMode()) {
+
+                } else {
+
+                }
+        }
+        return new InstructionList();
+    }
+
+    private InstructionList movInstructions(InstructionType inst, Register dst, Operand op) {
+        switch (inst) {
+            case MOV:
+                if (inAccumulatorMode()) {
+
+                } else {
+
+                }
+        }
+        return new InstructionList();
+    }
+
+    private InstructionList logicalInstructions(InstructionType inst, Register dst, Register src, Operand op) {
+        switch (inst) {
+            case AND:
+                if (inAccumulatorMode()) {
+
+                } else {
+
+                }
+        }
+        return new InstructionList();
+    }
+
+    private InstructionList arithmeticInstructions(InstructionType inst, Register dst, Register src, Operand op) {
+        // TODO: see if true for all cases
+        assert (op instanceof Register);
+
+        InstructionList result = new InstructionList();
+        if (inAccumulatorMode()) {
+            result.add(InstructionFactory.createPop(RESERVED));
+            src = RESERVED;
+        }
+        switch (inst) {
+            case ADD:
+                result.add(InstructionFactory.createAdd(dst, src, op));
+                break;
+            case ADDS:
+                result.add(InstructionFactory.createAdds(dst, src, op));
+                break;
+            case SUB:
+                result.add(InstructionFactory.createSub(dst, src, op));
+                break;
+            case SUBS:
+                result.add(InstructionFactory.createSubs(dst, src, (Register) op));
+                break;
+            case RSBS:
+                // TODO
+            case SMULL:
+                // TODO
+            default:
+                break;
+        }
+        return result;
+    }
+
+    private boolean inAccumulatorMode() {
+        return regOverflowCount > 0;
+    }
+
+    public InstructionList getInstructionList(InstructionType inst, Register dst, Register src, Operand op, Operand offset){
+        InstructionList result = new InstructionList();
+        if (inst.isStore()) {
+            return storeInstructions(inst, dst, src, offset);
+        } else if (inst.isLoad()) {
+            return loadInstructions(inst, dst, op, offset);
+        }
+        return result;
+    }
+
+    private InstructionList loadInstructions(InstructionType inst, Register dst, Operand op, Operand offset) {
+        return null;
+    }
+
+    private InstructionList storeInstructions(InstructionType inst, Register dst, Register src, Operand offset) {
+        return null;
+    }
 
 }
