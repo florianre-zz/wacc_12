f845507fbbf2c975ef996e3609a9965f797706bd
elyasaddo <les_a2do@yahoo.co.uk>
2015-12-02 22:11:07 +0000

    Add boolean functions to flag types of InstructionType Enums

--- src/main/java/arm11/InstructionType.java
+++ src/main/java/arm11/InstructionType.java
@@ -1,42 +1,49 @@
 package arm11;
 
 public enum InstructionType {
-  LDR("LDR"),
+  LTORG(".ltorg"),
+  TEXT("\n.text"),
+  GLOBAL("\n.global"),
+  WORD(".word"),
+  ASCII(".ascii"),
+  DATA(".data"),
+
   B("B"),
   BL("BL"),
   BEQ("BEQ"),
   BNE("BNE"),
   LABEL("label"),
-  PUSH("PUSH"),
-  POP("POP"),
-  LTORG(".ltorg"),
-  TEXT("\n.text"),
-  GLOBAL("\n.global"),
-  SUB("SUB"),
-  ADD("ADD"),
+
   MOV("MOV"),
-  WORD(".word"),
-  ASCII(".ascii"),
-  CMP("CMP"),
-  LDRNE("LDRNE"),
-  LDREQ("LDREQ"),
-  STR("STR"),
-  STRB("STRB"),
-  DATA(".data"),
-  LDRSB("LDRSB"),
-  EOR("EOR"),
-  RSBS("RSBS"),
-  ADDS("ADDS"),
   MOVEQ("MOVEQ"),
   MOVNE("MOVNE"),
   MOVGT("MOVGT"),
   MOVGE("MOVGE"),
   MOVLT("MOVLT"),
   MOVLE("MOVLE"),
+
+  LDR("LDR"),
+  LDRSB("LDRSB"),
+  LDRNE("LDRNE"),
+  LDREQ("LDREQ"),
+
+  STR("STR"),
+  STRB("STRB"),
+
+  PUSH("PUSH"),
+  POP("POP"),
+
+  ADD("ADD"),
+  ADDS("ADDS"),
+  SUB("SUB"),
+  SUBS("SUBS"),
+  RSBS("RSBS"),
+  SMULL("SMULL"),
+
   AND("AND"),
   ORR("ORR"),
-  SUBS("SUBS"),
-  SMULL("SMULL");
+  EOR("EOR"),
+  CMP("CMP");
 
   private final String name;
 
@@ -47,5 +54,70 @@ public enum InstructionType {
   @Override
   public String toString() {
     return "\t" + this.name;
+  }
+
+  public boolean isArithmetic() {
+    switch (this) {
+      case ADD:
+      case ADDS:
+      case SUB:
+      case SUBS:
+      case RSBS:
+      case SMULL: return true;
+      default: return false;
+    }
+  }
+
+
+  public boolean isLogical() {
+    switch (this) {
+      case AND:
+      case ORR:
+      case EOR:
+      case CMP:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+
+  public boolean isMove() {
+    switch (this) {
+      case MOV:
+      case MOVEQ:
+      case MOVGE:
+      case MOVGT:
+      case MOVLE:
+      case MOVLT:
+      case MOVNE:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+
+  public boolean isLoad() {
+    switch (this) {
+      case LDR:
+      case LDREQ:
+      case LDRNE:
+      case LDRSB:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+
+  public boolean isStore() {
+    switch (this) {
+      case STR:
+      case STRB:
+        return true;
+      default:
+        return false;
+    }
   }
 }
